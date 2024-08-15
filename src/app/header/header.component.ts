6583fbcc3e53a339fd7f33003168e19ab10e0f66
Ankur Papneja <ankurpapneja4@gmail.com>
2024-08-15 11:23:54 +0530

    Header Component - Added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-header',
+  template: '<h1>App Header Component</h1>'
+})
+export class HeaderComponent {
+
+}
